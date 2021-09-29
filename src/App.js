ae9c136708f92c1e848059e2231204fe25997058
Zane Russell <zane@zanes-mbp.lan>
2021-09-29 12:35:27 -0600

    working so far

--- src/App.js
+++ src/App.js
@@ -4,29 +4,35 @@ import { Informer } from "@consta/uikit/Informer";
 import { Theme, presetGpnDefault } from "@consta/uikit/Theme";
 import Routes from "./Routes";
 import { useAppContext } from "./context/app";
+import GoogleGranting from "./pages/google/GoogleGranting";
 
 function App() {
   const { globalError } = useAppContext();
   return (
-    <BrowserRouter>
-      <Theme preset={presetGpnDefault}>
-        <div className="App">
-          {globalError ? (
-            <div className="GlobalError">
-              <div style={{ height: 24 }} />
-              <Informer
-                status="alert"
-                view="filled"
-                title={globalError.title}
-                label={globalError.details}
-              />
-              <div style={{ height: 24 }} />
-            </div>
-          ) : null}
-          <Routes />
-        </div>
-      </Theme>
-    </BrowserRouter>
+    <div className='split'>
+      <span>
+        <GoogleGranting></GoogleGranting>
+      </span>
+      {/*<BrowserRouter>*/}
+      {/*  <Theme preset={presetGpnDefault}>*/}
+      {/*    <div className="App">*/}
+      {/*      {globalError ? (*/}
+      {/*        <div className="GlobalError">*/}
+      {/*          <div style={{ height: 24 }} />*/}
+      {/*          <Informer*/}
+      {/*            status="alert"*/}
+      {/*            view="filled"*/}
+      {/*            title={globalError.title}*/}
+      {/*            label={globalError.details}*/}
+      {/*          />*/}
+      {/*          <div style={{ height: 24 }} />*/}
+      {/*        </div>*/}
+      {/*      ) : null}*/}
+      {/*      <Routes />*/}
+      {/*    </div>*/}
+      {/*  </Theme>*/}
+      {/*</BrowserRouter>*/}
+    </div>
   );
 }
 
