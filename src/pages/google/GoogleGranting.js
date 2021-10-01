d49b39fb8eb4f82f7681c4768462fc0ca117da63
Zane Russell <zane@zanes-mbp.lan>
2021-10-01 11:31:38 -0600

    not quite

--- src/pages/google/GoogleGranting.js
+++ src/pages/google/GoogleGranting.js
@@ -6,8 +6,10 @@ import LitJsSdk from "lit-js-sdk";
 import { useAppContext } from "../../context";
 
 const API_HOST = process.env.REACT_APP_LIT_PROTOCOL_OAUTH_API_HOST;
+const GOOGLE_CLIENT_KEY = process.env.GOOGLE_CLIENT_ID
 
 export default function GoogleGranting() {
+  const gapi = window.gapi;
 
   const { performWithAuthSig } = useAppContext();
   // const [litNodeClient, setLitNodeClient] = useState({});
@@ -18,26 +20,52 @@ export default function GoogleGranting() {
   // const [token, setToken] = useState("");
   // const [accessControlConditions, setAccessControlConditions] = useState([]);
 
-  const connect = async (service) => {
-    await performWithAuthSig(async (authSig) => {
-      if (service === "google") {
-        const resp = await axios.post(`${API_HOST}/api/oauth/google/login`, {
-          authSig,
-        });
-        console.log('RES0', resp)
-        if (resp.data.redirectTo) {
-          window.location = resp.data.redirectTo;
+  // const connect = async (service) => {
+  //   await performWithAuthSig(async (authSig) => {
+  //     if (service === "google") {
+  //       const resp = await axios.post(`${API_HOST}/api/oauth/google/login`, {
+  //         authSig,
+  //       });
+  //       console.log('RES0', resp)
+  //       if (resp.data.redirectTo) {
+  //         window.location = resp.data.redirectTo;
+  //       }
+  //     }
+  //   });
+  // };
+
+  function authenticate() {
+    return gapi.auth2
+      .getAuthInstance()
+      .grantOfflineAccess()
+      .then(async (authResult) => {
+        console.log('AUTH RESULT', authResult)
+        if (authResult.code) {
+          var litNodeClient = new LitJsSdk.LitNodeClient();
+          await litNodeClient.connect();
+          // setLitNodeClient(litNodeClient);
+          // setToken(authResult.code);
+        } else {
+          console.log("Error logging in");
         }
-      }
+      });
+  }
+
+  gapi.load("client:auth2", function () {
+    console.log("LOAD")
+    gapi.auth2.init({
+      client_id: GOOGLE_CLIENT_KEY,
+      scope:
+        "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file",
     });
-  };
+  });
 
   return (
     <Theme preset={presetGpnDefault}>
       <div className="App">
         <Button
           label="Connect your Google account"
-          onClick={() => connect("google")}
+          onClick={() => authenticate("google")}
         />
       </div>
     </Theme>
