d1f5ecc13503ce76bfe7e9a4af7dc5e0f9a2d5c9
Zane Russell <zane@zanes-mbp.lan>
2021-10-22 10:29:33 -0600

    store connect service id

--- src/pages/sharedComponents/provisionAccess/ProvisionAccess.js
+++ src/pages/sharedComponents/provisionAccess/ProvisionAccess.js
@@ -6,10 +6,12 @@ import {
   DialogTitle, FormControl,
   IconButton,
   Input,
-  InputLabel, MenuItem, Select,
+  InputLabel,
+  MenuItem,
+  Select,
   TextField
 } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
+import { ShareModal } from "lit-access-control-conditions-modal";
 
 
 export default function ProvisionAccess(props) {
@@ -22,37 +24,54 @@ export default function ProvisionAccess(props) {
   }
 
   return (
-    <Dialog maxWidth={'md'} fullWidth={true} open={props.openProvisionAccessDialog}>
-      <DialogTitle className={'provision-access-header'}>
-        Provision Access
-      </DialogTitle>
-      <DialogContent>
-        <section className={'provision-access-container'}>
-          <p>Google Drive Link</p>
-          <FormControl fullWidth>
-            <Input className={'provision-access-input'} onChange={(e) => props.setLink(e.target.value)}/>
-          </FormControl>
-          {/*<TextField variants='standard' style={{ padding: '1rem'}} className={'provision-access-input'} focused />*/}
-          {/*<TextField variants='standard' style={{ padding: '1rem'}} className={'provision-access-input'} focused />*/}
-          <p>Permission Level</p>
-          {/*<Button onClick={() => handleSetRole}>YO</Button>*/}
-          <FormControl fullWidth>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={props.role}
-              onChange={handleSetRole}
-            >
-              <MenuItem value={0}>Read</MenuItem>
-              <MenuItem value={1}>Comment</MenuItem>
-              <MenuItem value={2}>Write</MenuItem>
-            </Select>
-          </FormControl>
-        </section>
-      </DialogContent>
-      <DialogActions>
-        <Button variant={'outlined'} onClick={() => props.handleCloseProvisionAccessDialog()}>Close</Button>
-      </DialogActions>
-    </Dialog>
-)
+    <section>
+      {!props.modalOpen && (
+        <Dialog maxWidth={'md'} fullWidth={true} open={props.openProvisionAccessDialog}>
+          <DialogTitle className={'provision-access-header'}>
+            Provision Access
+          </DialogTitle>
+          <DialogContent>
+            <section className={'provision-access-container'}>
+              <p>Google Drive Link</p>
+              <TextField fullWidth focused onChange={(e) => props.setLink(e.target.value)}/>
+              {/*<FormControl fullWidth>*/}
+              {/*  <Input className={'provision-access-input'} onChange={(e) => props.setLink(e.target.value)}/>*/}
+              {/*</FormControl>*/}
+              {/*<TextField variants='standard' style={{ padding: '1rem'}} className={'provision-access-input'} focused />*/}
+              {/*<TextField variants='standard' style={{ padding: '1rem'}} className={'provision-access-input'} focused />*/}
+              <p>Permission Level</p>
+              <FormControl fullWidth>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={props.role}
+                  onChange={handleSetRole}
+                >
+                  <MenuItem value={0}>Read</MenuItem>
+                  <MenuItem value={1}>Comment</MenuItem>
+                  <MenuItem value={2}>Write</MenuItem>
+                </Select>
+              </FormControl>
+            </section>
+          </DialogContent>
+          <DialogActions>
+            <Button variant={'outlined'} onClick={() => props.setModalOpen(true)}>Add Access Control Conditions</Button>
+            <Button variant={'outlined'} onClick={() => props.handleCloseProvisionAccessDialog()}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      )}
+      {props.modalOpen && (
+        <ShareModal
+          className={'share-modal'}
+          show={false}
+          onClose={() => props.setModalOpen(false)}
+          sharingItems={[{ name: props.link }]}
+          onAccessControlConditionsSelected={(restriction) => {
+            props.addToAccessControlConditions(restriction);
+            props.setModalOpen(false);
+          }}
+        />
+      )}
+    </section>
+  )
 }
--- src/pages/sharedComponents/serviceLinks/ServiceLinks.js
+++ src/pages/sharedComponents/serviceLinks/ServiceLinks.js
@@ -1,9 +1,8 @@
 import './ServiceLinks.scss';
 import {
   Button,
-  Card, IconButton,
+  Card, IconButton, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody
 } from "@mui/material";
-// import MUIDataTable from "mui-datatables";
 import EditIcon from '@mui/icons-material/Edit';
 import LinkIcon from '@mui/icons-material/Link';
 import DownloadIcon from '@mui/icons-material/Download';
@@ -13,83 +12,58 @@ export default function ServiceLinks(props) {
   const listOfLinks = props.listOfLinks;
   const serviceName = props.serviceName;
 
-  const columns = [
-    {
-      name: 'fileName',
-      label: 'File Name',
-      options: {
-        filter: true,
-        sort: true
-      }
-    },
-    {
-      name: 'requirements',
-      label: 'Requirements',
-      options: {
-        filter: true,
-        sort: true
-      }
-    },
-    {
-      name: 'fileType',
-      label: 'File Type',
-      options: {
-        filter: true,
-        sort: true
-      }
-    },
-    {
-      name: 'permission',
-      label: 'Permission',
-      options: {
-        filter: true,
-        sort: true
-      }
-    },
-    {
-      name: 'dateCreated',
-      label: 'Date Created',
-      options: {
-        filter: true,
-        sort: true
-      }
-    },
-    {
-      name: 'actions',
-      label: 'Actions',
-      options: {
-        filter: false,
-        sort: false,
-        customBodyRenderLite: (dataIndex, rowIndex) => {
-          return (
-            <span className={'links-actions'}>
-              <IconButton size={'small'}>
-                <EditIcon/>
-              </IconButton>
-              <IconButton size={'small'}>
-                <LinkIcon/>
-              </IconButton>
-              <IconButton size={'small'}>
-                <DownloadIcon/>
-              </IconButton>
-              <IconButton size={'small'}>
-                <DeleteIcon/>
-              </IconButton>
-            </span>
-          )
-        }
-      }
-    },
-  ]
+  // const columns = [
+  //   {
+  //     width: 200,
+  //     field: 'fileName',
+  //     headerName: 'File Name',
+  //   },
+  //   {
+  //     width: 200,
+  //     field: 'requirements',
+  //     headerName: 'Requirements',
+  //   },
+  //   {
+  //     width: 200,
+  //     field: 'fileType',
+  //     headerName: 'File Type',
+  //   },
+  //   {
+  //     width: 200,
+  //     field: 'permission',
+  //     headerName: 'Permission',
+  //   },
+  //   {
+  //     width: 200,
+  //     field: 'dateCreated',
+  //     headerName: 'Date Created',
+  //   },
+  //   {
+  //     width: 210,
+  //     field: 'actions',
+  //     headerName: 'Actions',
+  //     renderCell: (cellValues) => {
+  //       return (
+  //         <span className={'links-actions'}>
+  //             <IconButton size={'small'}>
+  //               <EditIcon/>
+  //             </IconButton>
+  //             <IconButton size={'small'}>
+  //               <LinkIcon/>
+  //             </IconButton>
+  //             <IconButton size={'small'}>
+  //               <DownloadIcon/>
+  //             </IconButton>
+  //             <IconButton size={'small'}>
+  //               <DeleteIcon/>
+  //             </IconButton>
+  //           </span>
+  //       )
+  //     }
+  //   },
+  // ]
 
-  const tableOptions = {
-    selectableRowsHideCheckboxes: true,
-    download: false,
-    print: false,
-    viewColumns: false,
-  }
-
-  if (listOfLinks && columns) {
+  if (listOfLinks) {
     return (
       <section>
         <Card className={'links-card'}>
@@ -97,12 +71,52 @@ export default function ServiceLinks(props) {
             <h3>Your {serviceName} Files</h3>
             <Button variant='outlined' onClick={() => props.handleOpenProvisionAccessDialog()}>Provision Access</Button>
           </span>
-          {/*TODO: remove 'material-ui/icons' and 'material-ui/core' from package.json once MUI Datatables is updated to use v5*/}
-          {/*<MUIDataTable*/}
-          {/*  columns={columns}*/}
-          {/*  data={listOfLinks}*/}
-          {/*  options={tableOptions}*/}
-          {/*/>*/}
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">File Name</TableCell>
+                  <TableCell align="left">Requirements</TableCell>
+                  <TableCell align="left">File Type</TableCell>
+                  <TableCell align="left">Permission</TableCell>
+                  <TableCell align="left">Date Created</TableCell>
+                  <TableCell align="left">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {listOfLinks.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.fileName}
+                    </TableCell>
+                    <TableCell align="left">{row.requirements}</TableCell>
+                    <TableCell align="left">{row.fileType}</TableCell>
+                    <TableCell align="left">{row.permission}</TableCell>
+                    <TableCell align="left">{row.dateCreated}</TableCell>
+                    <TableCell align="left">
+                      <span className={'links-actions'}>
+                        <IconButton size={'small'}>
+                          <EditIcon/>
+                        </IconButton>
+                        <IconButton size={'small'}>
+                          <LinkIcon/>
+                        </IconButton>
+                        <IconButton size={'small'}>
+                          <DownloadIcon/>
+                        </IconButton>
+                        <IconButton size={'small'}>
+                          <DeleteIcon/>
+                        </IconButton>
+                      </span>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Card>
       </section>
     )
