b47754ac210a5ab20022063cae6d39601dc586ee
Zane Russell <zane@zanes-mbp.lan>
2021-11-10 12:51:15 -0700

    postgres sql queries tightened for multiple users

--- src/pages/google/GoogleGranting.js
+++ src/pages/google/GoogleGranting.js
@@ -66,6 +66,9 @@ export default function GoogleGranting(props) {
         myWalletAddress: storedAuthSig.address,
       });
     };
+
+    console.log('ACCONDITI', accessControlConditions)
+
     humanizeAccessControlConditions().then(
       (humanizedAccessControlConditions) => {
         setHumanizedAccessControlArray(() => humanizedAccessControlConditions);
