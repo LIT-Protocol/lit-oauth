653e7baeabb59d92f750a796767619f5a65d9389
Zane Russell <zane@zanes-mbp.lan>
2021-11-15 16:54:51 -0700

    prettier turned on

--- src/pages/sharedComponents/backToApps/BackToApps.js
+++ src/pages/sharedComponents/backToApps/BackToApps.js
@@ -1,10 +1,10 @@
 import React from "react";
 import './BackToApps.scss';
 
-export default function BackToApps () {
+export default function BackToApps() {
   return (
     <a href={'https://dev.litgateway.com/apps'} className={'back-to-apps-container'}>
-      <div style={{ backgroundImage: `url('/appslogo.svg')`}} className={'back-to-apps-logo'}/>
+      <div style={{backgroundImage: `url('/appslogo.svg')`}} className={'back-to-apps-logo'}/>
       <h2>Lit Apps</h2>
     </a>
   )
