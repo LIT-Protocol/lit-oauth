a623bf10f954977be0eeee2cb78646a3a0669403
Chris Cassano <chris@litprotocol.com>
2021-11-18 13:40:13 -0800

    close modal after submitting so user doesnt leave page until share is stored for zoom

--- src/pages/zoom/ZoomGranting.js
+++ src/pages/zoom/ZoomGranting.js
@@ -5,7 +5,11 @@ import { useAppContext } from "../../context";
 import { Alert, CircularProgress, Snackbar } from "@mui/material";
 import ServiceHeader from "../sharedComponents/serviceHeader/ServiceHeader";
 import React, { useEffect, useState } from "react";
-import { createMeetingShare, getMeetingsAndWebinars, getServiceInfo } from "./zoomAsyncHelpers";
+import {
+  createMeetingShare,
+  getMeetingsAndWebinars,
+  getServiceInfo,
+} from "./zoomAsyncHelpers";
 import ZoomProvisionAccessModal from "./ZoomGrantingComponents/ZoomProvisionAccessModal";
 import { ShareModal } from "lit-access-control-conditions-modal";
 import { getResourceIdForMeeting, getSharingLink } from "./utils";
@@ -16,13 +20,14 @@ import BackToApps from "../sharedComponents/backToApps/BackToApps";
 const API_HOST = process.env.REACT_APP_LIT_PROTOCOL_OAUTH_API_HOST;
 
 export default function ZoomGranting() {
-  const {performWithAuthSig} = useAppContext();
+  const { performWithAuthSig } = useAppContext();
 
-  const [currentUser, setCurrentUser] = useState({})
+  const [currentUser, setCurrentUser] = useState({});
   const [allShares, setAllShares] = useState([]);
   const [currentServiceInfo, setCurrentServiceInfo] = useState(null);
   const [storedAuthSig, setStoredAuthSig] = useState({});
-  const [humanizedAccessControlArray, setHumanizedAccessControlArray] = useState([]);
+  const [humanizedAccessControlArray, setHumanizedAccessControlArray] =
+    useState([]);
   const [accessControlConditions, setAccessControlConditions] = useState([]);
   const [selectedMeeting, setSelectedMeeting] = useState(null);
   const [meetings, setMeetings] = useState([]);
@@ -34,7 +39,7 @@ export default function ZoomGranting() {
   const [snackbarInfo, setSnackbarInfo] = useState({});
 
   useEffect(() => {
-    console.log('PROCESS', process.env)
+    console.log("PROCESS", process.env);
     if (!!performWithAuthSig) {
       loadAuth();
     }
@@ -46,11 +51,13 @@ export default function ZoomGranting() {
         accessControlConditions,
         myWalletAddress: storedAuthSig.address,
       });
-    }
-    humanizeAccessControlConditions().then(humanizedAccessControlConditions => {
-      setHumanizedAccessControlArray(() => humanizedAccessControlConditions);
-    });
-  }, [accessControlConditions])
+    };
+    humanizeAccessControlConditions().then(
+      (humanizedAccessControlConditions) => {
+        setHumanizedAccessControlArray(() => humanizedAccessControlConditions);
+      }
+    );
+  }, [accessControlConditions]);
 
   const handleAddAccessControl = async () => {
     setOpenShareModal(true);
@@ -58,9 +65,9 @@ export default function ZoomGranting() {
   };
 
   const handleGetShareLink = async () => {
+    await handleSubmit();
     setOpenProvisionAccessDialog(false);
     setOpenShareModal(false);
-    await handleSubmit();
   };
 
   const handleOpenProvisionAccessDialog = async () => {
@@ -77,8 +84,8 @@ export default function ZoomGranting() {
   const handleOpenSnackBar = (message, severity) => {
     setSnackbarInfo({
       message: message,
-      severity: severity
-    })
+      severity: severity,
+    });
     setOpenSnackbar(true);
   };
 
@@ -93,27 +100,27 @@ export default function ZoomGranting() {
     await performWithAuthSig(async (authSig) => {
       await setStoredAuthSig(authSig);
 
-      if (!storedAuthSig || !storedAuthSig['sig']) {
-        console.log('Stop auth if authSig is not yet available');
+      if (!storedAuthSig || !storedAuthSig["sig"]) {
+        console.log("Stop auth if authSig is not yet available");
         return;
       }
 
-      console.log('STORED AUTH SIG', storedAuthSig)
+      console.log("STORED AUTH SIG", storedAuthSig);
 
       const serviceInfo = await getServiceInfo(storedAuthSig);
       // if previous connection exists, retrieve it from DB
       if (serviceInfo?.data[0]) {
-        console.log('SERVICE INFO', serviceInfo.data[0])
+        console.log("SERVICE INFO", serviceInfo.data[0]);
         setCurrentServiceInfo(serviceInfo.data[0]);
         // await loadMeetings(storedAuthSig);
-        await setUserProfile(serviceInfo.data[0])
-        await getAllShares(storedAuthSig)
+        await setUserProfile(serviceInfo.data[0]);
+        await getAllShares(storedAuthSig);
       } else {
         // if no connection is saved, connect to zoom
         await connectToZoom(storedAuthSig);
       }
-    })
-  }
+    });
+  };
 
   const setUserProfile = async (currentUserObject) => {
     const userBasicProfile = currentUserObject;
@@ -122,10 +129,10 @@ export default function ZoomGranting() {
       email: userBasicProfile.email,
       displayName: userBasicProfile.email,
       givenName: userBasicProfile.email,
-      avatar: 'Z',
+      avatar: "Z",
     };
     setCurrentUser(userProfile);
-  }
+  };
 
   const connectToZoom = async (authSig) => {
     const resp = await axios.post(`${API_HOST}/api/oauth/zoom/serviceLogin`, {
@@ -134,11 +141,11 @@ export default function ZoomGranting() {
     if (resp.data.redirectTo) {
       window.location = resp.data.redirectTo;
     }
-  }
+  };
 
   const loadMeetings = async (authSig) => {
-    console.log('start of meetings and webinars')
-    const resp = await getMeetingsAndWebinars({authSig});
+    console.log("start of meetings and webinars");
+    const resp = await getMeetingsAndWebinars({ authSig });
 
     // const flatMeetings = resp.meetings.map((m) => m.meetings).flat();
     // const flatWebinars = resp.webinars.map((m) => m.webinars).flat();
@@ -148,19 +155,19 @@ export default function ZoomGranting() {
   const getAllShares = async (authSig) => {
     const allSharesHolder = await asyncHelpers.getAllShares(authSig);
 
-    const humanizeAccPromiseArray = allSharesHolder.data.map(s => {
+    const humanizeAccPromiseArray = allSharesHolder.data.map((s) => {
       const shareAcConditions = JSON.parse(s.accessControlConditions);
       return LitJsSdk.humanizeAccessControlConditions({
         accessControlConditions: shareAcConditions,
         myWalletAddress: storedAuthSig.address,
-      })
+      });
     });
 
-    Promise.all(humanizeAccPromiseArray).then(humanizedAcc => {
+    Promise.all(humanizeAccPromiseArray).then((humanizedAcc) => {
       let combinedAllShares = [];
       for (let i = 0; i < allSharesHolder.data.length; i++) {
         let singleShare = allSharesHolder.data[i];
-        singleShare['humanizedAccessControlConditions'] = humanizedAcc[i];
+        singleShare["humanizedAccessControlConditions"] = humanizedAcc[i];
         combinedAllShares.push(singleShare);
       }
       setAllShares(allSharesHolder.data.reverse());
@@ -182,7 +189,7 @@ export default function ZoomGranting() {
   };
 
   const signOut = async () => {
-    console.log('currentServiceInfo', currentServiceInfo)
+    console.log("currentServiceInfo", currentServiceInfo);
     // logout(currentServiceInfo.email).then((res) => {
     setAccessControlConditions([]);
     setCurrentUser({});
@@ -211,14 +218,16 @@ export default function ZoomGranting() {
       setSelectedMeeting(meeting);
       const share = meeting.shares[0] ?? resp.data[0];
 
-      console.log('SELECTED MEETING', share)
+      console.log("SELECTED MEETING", share);
 
       const resourceId = getResourceIdForMeeting({
-        meeting: {id: share.id},
+        meeting: { id: share.id },
         share,
       });
 
-      const parsedAccessControlConditions = JSON.parse(share.accessControlConditions);
+      const parsedAccessControlConditions = JSON.parse(
+        share.accessControlConditions
+      );
 
       await window.litNodeClient.saveSigningCondition({
         accessControlConditions: parsedAccessControlConditions,
@@ -237,57 +246,60 @@ export default function ZoomGranting() {
     try {
       await asyncHelpers.deleteShare(shareInfo.id);
       await getAllShares(storedAuthSig);
-      handleOpenSnackBar(`${shareInfo.name} has been deleted.`, 'success');
+      handleOpenSnackBar(`${shareInfo.name} has been deleted.`, "success");
     } catch (err) {
-      console.log(`'Error deleting share', ${err}`)
-      handleOpenSnackBar(`Error deleting share: ${err}`, 'error');
+      console.log(`'Error deleting share', ${err}`);
+      handleOpenSnackBar(`Error deleting share: ${err}`, "error");
     }
   };
 
   const getLinkFromShare = async (share) => {
-    console.log('SHARE!', share)
+    console.log("SHARE!", share);
     const link = getSharingLink(share);
     setSnackbarInfo({
       message: `Link has been copied to clipboard.`,
-      severity: 'info'
-    })
+      severity: "info",
+    });
     setOpenSnackbar(true);
-    await navigator.clipboard.writeText(link)
+    await navigator.clipboard.writeText(link);
   };
 
   return (
     <div>
-      <BackToApps/>
-      {(!storedAuthSig['sig'] || !currentServiceInfo) ? (
-        <div className={'service-loader'}>
-          <CircularProgress/>
+      <BackToApps />
+      {!storedAuthSig["sig"] || !currentServiceInfo ? (
+        <div className={"service-loader"}>
+          <CircularProgress />
           <h3>Waiting for Zoom - Ensure Pop-ups are enabled</h3>
         </div>
       ) : (
         <section className={"service-grid-container"}>
-          <div className={'service-grid-header'}>
+          <div className={"service-grid-header"}>
             <ServiceHeader
               serviceName={"Zoom App"}
               oauthServiceProvider={"Zoom"}
               currentUser={currentUser}
-              serviceImageUrl={'/zoom.png'}
+              serviceImageUrl={"/zoom.png"}
               signOut={signOut}
             />
           </div>
-          <div className={'service-grid-links'}>
+          <div className={"service-grid-links"}>
             <h3>Zoom Meetings and Webinars</h3>
             <ZoomMeetings
               className={"service-links"}
               serviceName={"Drive"}
               handleOpenProvisionAccessDialog={handleOpenProvisionAccessDialog}
-              handleEditLinkAction={() => console.log('EDIT CLICKED')}
+              handleEditLinkAction={() => console.log("EDIT CLICKED")}
               handleCopyLinkAction={(share) => getLinkFromShare(share)}
-              handleDownloadLinkAction={() => console.log('DOWNLOAD CLICKED')}
+              handleDownloadLinkAction={() => console.log("DOWNLOAD CLICKED")}
               handleDeleteLinkAction={(share) => handleDeleteShare(share)}
-              listOfShares={allShares}/>
+              listOfShares={allShares}
+            />
           </div>
           <ZoomProvisionAccessModal
-            handleCancelProvisionAccessDialog={handleCancelProvisionAccessDialog}
+            handleCancelProvisionAccessDialog={
+              handleCancelProvisionAccessDialog
+            }
             accessControlConditions={accessControlConditions}
             removeIthAccessControlCondition={removeIthAccessControlCondition}
             setAccessControlConditions={setAccessControlConditions}
@@ -318,24 +330,31 @@ export default function ZoomGranting() {
             />
           )}
           <LitProtocolConnection
-            className={'lit-protocol-connection'}
-            connection={!!storedAuthSig['sig']}/>
-          {process.env.NODE_ENV === 'development' && (
-            <button style={{position: 'absolute', top: '0', left: '0'}}
-                    onClick={async () => {
-                      const resp = await axios.post(`${API_HOST}/api/zoom/deleteUser`, {
-                        address: storedAuthSig.address,
-                        idOnService: currentServiceInfo.idOnService
-                      });
+            className={"lit-protocol-connection"}
+            connection={!!storedAuthSig["sig"]}
+          />
+          {process.env.NODE_ENV === "development" && (
+            <button
+              style={{ position: "absolute", top: "0", left: "0" }}
+              onClick={async () => {
+                const resp = await axios.post(
+                  `${API_HOST}/api/zoom/deleteUser`,
+                  {
+                    address: storedAuthSig.address,
+                    idOnService: currentServiceInfo.idOnService,
+                  }
+                );
 
-                      console.log('DELETED', resp);
-                    }}>DELETE USER
+                console.log("DELETED", resp);
+              }}
+            >
+              DELETE USER
             </button>
           )}
         </section>
       )}
       <Snackbar
-        anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
         open={openSnackbar}
         autoHideDuration={5000}
         onClose={handleCloseSnackbar}
@@ -343,5 +362,5 @@ export default function ZoomGranting() {
         <Alert severity={snackbarInfo.severity}>{snackbarInfo.message}</Alert>
       </Snackbar>
     </div>
-  )
+  );
 }
