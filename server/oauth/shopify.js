fdd1a511ffcd8dd0a60b21c0040eec9da27ad4be
Zane Russell <zane@zanes-mbp.lan>
2021-12-05 11:41:38 -0700

    update to test shopify connect

--- server/oauth/shopify.js
+++ server/oauth/shopify.js
@@ -5,22 +5,66 @@ export default async function (fastify, opts) {
   fastify.post('/api/shopify/saveCondition', async (request, reply) => {
     console.log('shopify test')
 
-    return 'success connecting to lit shopify locally';
+    const insertToLinksQuery = await fastify.objection.models.shopify_shares
+      .query()
+      .insert({
+        asset_id_on_service: request.body.driveId,
+        access_control_conditions: JSON.stringify(
+          request.body.accessControlConditions
+        ),
+        connected_service_id: request.body.connectedServiceId,
+        role: request.body.role,
+        user_id: request.body.authSig.address,
+        name: request.body.name,
+        asset_type: request.body.assetType,
+        extra_data: request.body.extraData,
+      });
+
+    let uuid = await insertToLinksQuery.id;
+
+    return {
+      authorizedControlConditions: request.body.accessControlConditions,
+      uuid,
+    };
   })
 
-  fastify.post('/api/shopify/getCustomer', async (request, reply) => {
+  fastify.post('/api/shopify/getDiscounts', async (request, reply) => {
 
-    console.log('get Customer')
+    const {storeName} = request.body.extraData
 
-    return 'success connecting to get customer';
+    const discounts = await fastify.objection.models.shopify_shares
+      .query()
+      .where('user_id', request.body.authSig.address)
+
+    const filteredDiscounts = discounts.filter(d => storeName === d.extraData.storeName);
+
+    return filteredDiscounts;
+  })
+
+  fastify.post('/api/shopify/testCondition', async (request, reply) => {
+
+    console.log('request connected')
+
+    return true;
   })
 
   fastify.post('/api/shopify/deleteCustomer', async (request, reply) => {
 
-    console.log('delete customer shopify')
+    const {address, idOnService} = request.body;
+    const shareResponse = await fastify.objection.models.shopify_shares
+      .query()
+      .delete()
+      .where("user_id", "=", address)
+      .where('asset_id_on_service', '=', idOnService)
 
-    return 'success connecting to delete customer';
-  })
+    // const response = await fastify.objection.models.connectedServices
+    //   .query()
+    //   .delete()
+    //   .where("user_id", "=", address)
+    //   .where("id_on_service", "=", idOnService);
+
+    return reply;
+  });
 
   fastify.post('/api/shopify/deleteStore', async (request, reply) => {
 
