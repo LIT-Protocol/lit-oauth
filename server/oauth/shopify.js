995b6ca90ce3050a6810acd84a3b09d52991a3f9
zanedr <zanedanielrussell@gmail.com>
2022-02-04 14:57:54 -0700

    log for shop variables

--- server/oauth/shopify.js
+++ server/oauth/shopify.js
@@ -15,7 +15,7 @@ export default async function (fastify, opts) {
   // NEW_SECTION: save auth
 
   fastify.post("/api/shopify/saveAccessToken", async (request, reply) => {
-    console.log('Check start of save access token')
+    console.log('Check start of save access token', request.body)
     const { shop, accessToken, email } = request.body;
     const shortenedShopName = shortenShopName(shop);
     const queryForExistingShop = await fastify.objection.models.shopifyStores
