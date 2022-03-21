b79d230732fb2c266bdca03e7f5d929aab94abf2
zanedr <zanedanielrussell@gmail.com>
2022-03-20 20:27:53 -0600

    update rh file

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import googleOauthEndpoints from "./oauth/google.js";
 import shopifyEndpoints from "./oauth/shopify.js";
 import shopifyDoodlesEndpoints from "./oauth/customShopifyEndpoints/shopifyDoodles.js";
 import shopifyMetarelicEndpoints from "./oauth/customShopifyEndpoints/shopifyMetarelic.js";
-import shopifyRhEndpoints from "./oauth/customShopifyEndpoints/shopifyRh";
+import shopifyRhEndpoints from "./oauth/customShopifyEndpoints/shopifyRh.js";
 import knexConfig from "./knexfile.js";
 import Bugsnag from "@bugsnag/js";
 
