216a504a38432cb36e1a3115e68ad9ed148ed988
zanedr <zanedanielrussell@gmail.com>
2022-04-05 11:06:50 -0600

    add logging to poap draft orders

--- server/oauth/customShopifyEndpoints/shopifyPoapEndpoints.js
+++ server/oauth/customShopifyEndpoints/shopifyPoapEndpoints.js
@@ -162,7 +162,7 @@ export default async function shopifyPoapEndpoints(fastify, opts) {
       const draftOrders = await fastify.objection.models.shopifyDraftOrders
         .query()
         .where("shop_id", "=", request.body.shopId);
-      // console.log('draftOrders', draftOrders)
+      console.log('poap draftOrders', draftOrders)
 
       return draftOrders;
     } catch (err) {
