05e929a09a185a9a858b67dec9bc0ada72452b27
zanedr <zanedanielrussell@gmail.com>
2022-04-11 18:58:58 -0600

    google fix

--- server/oauth/googleHelpers.js
+++ server/oauth/googleHelpers.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const validateJWT = async (token) => {
-  console.log('check validateJWT', token, process.env.REACT_APP_LIT_PROTOCOL_OAUTH_GOOGLE_CLIENT_SECRET);
   const result = jwt.verify(token, process.env.REACT_APP_LIT_PROTOCOL_OAUTH_GOOGLE_CLIENT_SECRET);
   return result;
 }
