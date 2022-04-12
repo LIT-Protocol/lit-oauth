9c2a3ee05341a0140782420f9ddefcb70b208b26
zanedr <zanedanielrussell@gmail.com>
2022-04-11 18:48:26 -0600

    change jwt import

--- server/oauth/googleHelpers.js
+++ server/oauth/googleHelpers.js
@@ -1,8 +1,7 @@
-// import jwt from "jsonwebtoken";
-import { verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const validateJWT = async (token) => {
   console.log('check validateJWT', token, process.env.REACT_APP_LIT_PROTOCOL_OAUTH_GOOGLE_CLIENT_SECRET);
-  const result = verify(token, process.env.REACT_APP_LIT_PROTOCOL_OAUTH_GOOGLE_CLIENT_SECRET);
+  const result = jwt.verify(token, process.env.REACT_APP_LIT_PROTOCOL_OAUTH_GOOGLE_CLIENT_SECRET);
   return result;
 }
