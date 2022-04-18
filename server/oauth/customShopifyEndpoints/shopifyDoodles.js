49660a987a5c8f1087a1b5621126060ba8f09629
zanedr <zanedanielrussell@gmail.com>
2022-04-18 16:21:10 -0600

    remove duplicate endpoints

--- server/oauth/customShopifyEndpoints/shopifyDoodles.js
+++ server/oauth/customShopifyEndpoints/shopifyDoodles.js
@@ -230,7 +230,7 @@ export default async function shopifyDoodlesEndpoints(fastify, opts) {
     }
   });
 
-  fastify.post("/api/shopify/deleteDoodlesDraftOrder", async (request, reply) => {
+  fastify.post("/api/shopify/checkOnDoodlesStores", async (request, reply) => {
     const name = request.body;
     const allResults = fastify.objection.models.shopifyStores
       .query()
