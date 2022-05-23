d7cb3ba62447ffd70f10041045d9f146337e3281
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-05-23 15:55:23 -0700

    adding columns

---
+++ server/migrations/20220523195449_add_dao_address_to_shares.js
@@ -0,0 +1,13 @@
+export const up = (knex) => {
+  return knex.schema.table("shares", (table) => {
+    table.string("dao_address").index();
+    table.string("source");
+  });
+};
+
+export const down = (knex) => {
+  return knex.schema.table("shares", (table) => {
+    table.dropColumn("dao_address");
+    table.dropColumn("source");
+  });
+};
