e241202e2a8e39f24bf1607e294a8dacce0c24a8
zanedr <zanedanielrussell@gmail.com>
2022-07-27 16:25:57 -0600

    switch new migration to shares

--- server/migrations/20220727221224_recreate_extra_data.js
+++ server/migrations/20220727221224_recreate_extra_data.js
@@ -1,11 +1,11 @@
 export const up = (knex) => {
-  return knex.schema.createTable("connected_services", (table) => {
+  return knex.schema.createTable("shares", (table) => {
     table.text("extra_data");
   });
 };
 
 export const down = (knex) => {
-  return knex.schema.table('connected_services', table => {
+  return knex.schema.table('shares', table => {
     table.dropColumn('redeemed_by');
   })
 };
