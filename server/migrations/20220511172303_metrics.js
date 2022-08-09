b1d14c7aa85dcc6e6189001fb2779b0910eded74
zanedr <zanedanielrussell@gmail.com>
2022-08-09 12:46:50 -0600

    update metric migration

--- server/migrations/20220511172303_metrics.js
+++ server/migrations/20220511172303_metrics.js
@@ -5,7 +5,12 @@ export const up = (knex) => {
       .unique()
       .primary()
       .defaultTo(knex.raw("gen_random_uuid()"));
-    table.text("shopify_draft_order_data")
+    table.text("store_name")
+    table.text("store_id")
+    table.text("offer_uuid")
+    table.text("list_of_redemptions")
+    table.text("draft_order_details")
+    table.text("asset_id_on_service")
     table.timestamps();
   });
 };
